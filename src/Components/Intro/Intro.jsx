0a51222f8fca2e7eb5d9d37c3bde00cb78892b9f
freetimezp <kraftukrnet@ukr.net>
2022-11-16 00:48:58 +0200

    04. js-portfolio-07-web-dev. Intro component, Right side.

--- src/Components/Intro/Intro.jsx
+++ src/Components/Intro/Intro.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 
+import FloatingDiv from '../FloatingDiv/FloatingDiv';
+
 import Github from '../../img/github.png';
 import LonkedIn from '../../img/linkedin.png';
 import Instagram from '../../img/instagram.png';
 
+import Vector1 from '../../img/Vector1.png';
+import Vector2 from '../../img/Vector2.png';
+import Boy from '../../img/boy.png';
+import Thumbup from '../../img/thumbup.png';
+import Crown from '../../img/crown.png';
+import Glassesimoji from '../../img/glassesimoji.png';
 
 import './Intro.css';
 
@@ -29,7 +37,20 @@ const Intro = () => {
             </div>
         </div>
         <div className="i-right">
-            right
+            <img src={Vector1} alt="vector" />
+            <img src={Vector2} alt="vector" />
+            <img src={Boy} alt="boy" />
+            <img src={Glassesimoji} alt="Glassesimoji" />
+
+            <div style={{top: '-4%', left: '68%'}}>
+                <FloatingDiv image={Crown} txt1="Web" txt2="Developer" />
+            </div>
+            <div style={{top: '18rem', left: '0'}}>
+                <FloatingDiv image={Thumbup} txt1="Your" txt2="Best Design" />
+            </div>
+
+            <div className="blur"></div>
+            <div className="blur blur2"></div>
         </div>
     </div>
   );
